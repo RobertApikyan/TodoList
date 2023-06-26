48849291f76fa7155d3405a201035fbae4c27231
Robert Apikyan <robert.apikyan@volo.global>
2023-06-26 15:50:57 +0400

    Added color shades

--- lib/styles/app_color.dart
+++ lib/styles/app_color.dart
@@ -1,5 +1,8 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 class AppColors {
   static const backgroundColor = Color(0xffF9F8F4);
   static const primaryColor = Color(0xff3E9EC8);
@@ -8,4 +11,37 @@ class AppColors {
   static const selectedCardColor = Color(0xffE8F5FF);
   static const white = Color(0xffffffff);
   static const black = Color(0xff000000);
+
+  // static const MaterialColor blue = MaterialColor(
+  //   _bluePrimary,
+  //   <int, Color>{
+  //     0 : Color(0xFFffffff),
+  //     10: Color(0xFFe6eff3),
+  //     20: Color(0xFFccdfe7),
+  //     30: Color(0xFFb3ceda),
+  //     40: Color(0xFF99bece),
+  //     50: Color(0xFF80aec2),
+  //     60: Color(_bluePrimary),
+  //     70: Color(0xFF4d8eaa),
+  //     80: Color(0xFF337d9d),
+  //     90: Color(0xFF1a6d91),
+  //     100: Color(0xFF005d85),
+  //   },
+  // );
+  // static const int _bluePrimary = 0xFF669eb6;
+  //
+  // static const black = MaterialColor(_blackPrimary, <int, Color>{
+  //   0: Color(0xFFffffff),
+  //   10: Color(0xFFe6e6e6),
+  //   20: Color(0xFFcccccc),
+  //   30: Color(0xFFb3b3b3),
+  //   40: Color(0xFF999999),
+  //   50: Color(0xFF808080),
+  //   60: Color(0xFF666666),
+  //   70: Color(0xFF4d4d4d),
+  //   80: Color(0xFF333333),
+  //   90: Color(0xFF1a1a1a),
+  //   100: Color(_blackPrimary),
+  // });
+  // static const int _blackPrimary = 0xFF000000;
 }
