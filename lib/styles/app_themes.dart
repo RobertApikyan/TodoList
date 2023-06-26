f4c6bfe569acd7095242bb92a3b9d0e5315005dc
Robert Apikyan <robert.apikyan@volo.global>
2023-06-26 18:26:02 +0400

    Themes adjusted

--- lib/styles/app_themes.dart
+++ lib/styles/app_themes.dart
@@ -15,12 +15,12 @@ class AppThemes {
 
   static dark() => ThemeData(
       primaryColor: AppColors.blue,
-      appBarTheme: const AppBarTheme(backgroundColor: AppColors.blue),
+      appBarTheme: AppBarTheme(backgroundColor: AppColors.blue[100]),
       scaffoldBackgroundColor: AppColors.black[90],
       checkboxTheme: CheckboxThemeData(
         checkColor: MaterialStateProperty.all(AppColors.black[0]),
-        fillColor: MaterialStateProperty.all(AppColors.blue[80]),
+        fillColor: MaterialStateProperty.all(AppColors.blue[90]),
       ),
       floatingActionButtonTheme:
-          const FloatingActionButtonThemeData(backgroundColor: AppColors.blue));
+          FloatingActionButtonThemeData(backgroundColor: AppColors.blue[100]));
 }
